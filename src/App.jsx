db93fd9f946bc8219e9b27717e119cee2a658dc5
kokhinmaungwin <khinmaungwin675@gmail.com>
2025-12-08 17:21:57 +0630

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CodeEditorApp from "./CodeEditorApp";
+
+export default function App() {
+  return (
+    <div className="p-4 text-white bg-gray-900 min-h-screen">
+      <CodeEditorApp />
+    </div>
+  );
+}
