97e405e9fa24a50fb3cc9acfd7104992c61f3dd5
kokhinmaungwin <khinmaungwin675@gmail.com>
2025-12-08 17:47:48 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Code-Editor-App/",   // IMPORTANT for GitHub Pages
+  base: "/Code-Editor-App/",
 });
